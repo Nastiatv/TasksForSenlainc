b02893a15ce7dd66db5f8a9a348a94b08d220f6c
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-16 14:50:38 +0300

    +

--- Task6/Check.java
+++ Task6/Check.java
@@ -3,26 +3,30 @@ package Task6;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Check {
 
 	public static void main(String[] args) {
 
 		Bag firstBag = new Bag(10);
 
-		
-		
 		List<Stuff> stuffList = new ArrayList<Stuff>();
-		
-		
+
 		stuffList.add(new Stuff("phone", 1, 100));
-		stuffList.add(new Stuff("laptop", 4, 200));
+		stuffList.add(new Stuff("laptop", 5, 200));
 		stuffList.add(new Stuff("key", 3, 200));
 		stuffList.add(new Stuff("books", 3, 50));
 		stuffList.add(new Stuff("food", 2, 20));
-	
+
 		firstBag.setStuffList(stuffList);
 
 		firstBag.packBag(firstBag.getStuffList());
 
+	System.out.println("Current Weight: "+firstBag.getCurrentWeight());
+	System.out.println("Stuff in Bag:");
+	for(Stuff i:firstBag.getStuffInBag()) {
+		System.out.println("-"+i.getName()+" , value: "+i.getValue());
+	}
+		
 	}
 }
