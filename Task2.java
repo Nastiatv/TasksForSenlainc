7e8cca1a79389e2dcc9a30eb22ab31210d061d52
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-20 11:25:33 +0300

    +

--- Task2.java
+++ Task2.java
@@ -1,7 +1,7 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task2 {
-
 	/*
 	 * Создать программу, которая будет вычислять и выводить на экран НОК
 	 * (наименьшее общее кратное) и НОД (наибольший общий делитель) двух целых
@@ -9,29 +9,20 @@ public class Task2 {
 	 * одно из чисел, то сообщать об ошибке.
 	 */
 
-	private static final String eror = "mistake! You entered not a integer";
-
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		System.out.println("Insert first number");
 		Scanner scan = new Scanner(System.in);
-		if (scan.hasNextInt()) {
+		try {
 			int first = scan.nextInt();
 			System.out.println("Insert second number");
-			if (scan.hasNextInt()) {
-				int second = scan.nextInt();
+			int second = scan.nextInt();
+			scan.close();
+			System.out.println("NOD=" + countNOD(first, second));
+			System.out.println("NOK=" + countNOК(first, second));
 
-				System.out.println("NOD=" + countNOD(first, second));
-				System.out.println("NOK=" + countNOК(first, second));
-
-			} else {
-				System.out.println(eror);
-			}
-					} else {
-			System.out.println(eror);
+		} catch (InputMismatchException e) {
+			System.out.println("mistake! You entered not a integer");
 		}
-		
-
 	}
 
 	private static int countNOК(int one, int two) {
