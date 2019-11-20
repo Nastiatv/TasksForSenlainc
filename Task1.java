01164ee5e989d6ec5f5df808b02c43ab7795cc11
Nastiatv <57249641+Nastiatv@users.noreply.github.com>
2019-11-20 11:21:42 +0300

    +

--- Task1.java
+++ Task1.java
@@ -7,6 +7,21 @@ public class Task1 {
 	 * пользователем, чётным или нечётным, простым или составным. Если пользователь
 	 * введёт не целое число, то сообщать ему об ошибке.
 	 */
+	public static void evenOrOdd(int num) {
+		String b = (num % 2 == 0 ? "even" : "odd");
+		System.out.print(b);
+	}
+
+	public static void compositeOrPrime(int num) {
+		int n = 0;
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				n++;
+			}
+		}
+		String a = (n != 0 ? "The number is composite and " : "The number is prime and ");
+		System.out.print(a);
+	}
 
 	public static void main(String[] args) {
 
@@ -14,20 +29,10 @@ public class Task1 {
 		System.out.println("Insert the number");
 		try {
 			int num = scan.nextInt();
-			int n = 0;
-
-			for (int i = 2; i < num; i++) {
-				if (num % i == 0) {
-					n++;
-				}
-			}
-			String a = (n != 0 ? "The number is composite and " : "The number is prime and ");
-			System.out.print(a);
-
-			String b = (num % 2 == 0 ? "even" : "odd");
-			System.out.print(b);
-
+			compositeOrPrime(num);
+			evenOrOdd(num);
 			scan.close();
+
 		} catch (InputMismatchException e) {
 			System.out.println("mistake! You entered not a integer");
 		}
